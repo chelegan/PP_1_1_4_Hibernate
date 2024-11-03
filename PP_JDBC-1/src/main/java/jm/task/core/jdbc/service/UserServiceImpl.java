05bac39eecc8aa44afaa1205b41cfe1504bf7d0a
chelegan <89045101525@mail.ru>
2024-11-03 18:45:13 +0300

    С ПК

--- PP_JDBC-1/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_JDBC-1/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,35 +8,35 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private UserDaoHibernateImpl userDaoHybernate = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        userDaoHybernate.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+        userDaoHybernate.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name, lastName, age);
+        userDaoHybernate.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        userDaoHybernate.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userDaoJDBC.getAllUsers();
+        return userDaoHybernate.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        userDaoHybernate.cleanUsersTable();
     }
 }
